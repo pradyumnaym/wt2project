1ce43f4953a65f1f5bf0fcb3cb0cb715cfd41e6c
PruthvishE <pruthvish.eshwar@mindtree.com>
2020-04-12 03:41:10 +0530

    blog backend

--- angular-gaming/src/app/app-routing.module.ts
+++ angular-gaming/src/app/app-routing.module.ts
@@ -6,14 +6,16 @@ import { ProfileComponent } from './profile/profile.component';
 import {AuthGuard} from './guards/auth.guard';
 import { HomeComponent } from './home/home.component';
 import {RequestsComponent} from './requests/requests.component';
-
+import {BlogComponent} from './blog/blog.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]},
-  {path: 'requests', component: RequestsComponent,canActivate: [AuthGuard]}
+  {path: 'requests', component: RequestsComponent,canActivate: [AuthGuard]},
+  {path: 'blog', component: BlogComponent}
+
 ];
 
 @NgModule({
