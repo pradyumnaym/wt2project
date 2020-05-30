1d892e970de7e10507efe0b5b2dbde66ea155175
Pruthvish-E <umapruthvish@gmail.com>
2020-05-30 12:03:19 +0530

    system testing done

--- system-testing/test_before_login.py
+++ system-testing/test_before_login.py
@@ -1,6 +1,8 @@
 import unittest
 from selenium import webdriver
 import time
+from random import randint
+
 
 class SearchText(unittest.TestCase):
     def setUp(self):
@@ -11,6 +13,52 @@ class SearchText(unittest.TestCase):
         self.driver.maximize_window()
         # navigate to the application home page
         self.driver.get("http://localhost:4200/")
+
+
+    def test_register(self):
+        self.driver.get("http://localhost:4200/register/")
+
+        randnum = randint(400,2000)
+
+        firstname = self.driver.find_element_by_name("firstname")
+        firstname.send_keys("tester"+str(randnum))
+
+        lastname = self.driver.find_element_by_name("lastname")
+        lastname.send_keys("tester"+str(randnum))
+
+        email = self.driver.find_element_by_name("email")
+        email.send_keys("tester"+str(randnum)+"@g.com")
+
+        username = self.driver.find_element_by_name("username")
+        username.send_keys("tester"+str(randnum))
+
+        password = self.driver.find_element_by_name("password")
+        password.send_keys("tester"+str(randnum))
+
+        self.driver.find_element_by_xpath("//input[@type = 'submit']").click()
+
+        time.sleep(1)
+
+        self.driver.get("http://localhost:4200/login/")
+
+        self.search_field = self.driver.find_element_by_name("username")
+
+        self.search_field.send_keys("tester"+str(randnum))
+
+        self.search_field = self.driver.find_element_by_name("password")
+
+        self.search_field.send_keys("tester"+str(randnum))
+        self.search_field.submit()
+
+        
+        time.sleep(2)
+        url_redirect = self.driver.current_url
+
+        self.assertEqual(url_redirect, "http://localhost:4200/home")
+
+
+
+        
 
     def test_invalid_login(self):
        
