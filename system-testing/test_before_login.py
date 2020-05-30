d085ec6560f0f16db8f7c3b4933f9f3050e9a76c
Pruthvish-E <umapruthvish@gmail.com>
2020-05-30 07:24:19 +0530

    system testing

--- system-testing/test_before_login.py
+++ system-testing/test_before_login.py
@@ -12,7 +12,7 @@ class SearchText(unittest.TestCase):
         # navigate to the application home page
         self.driver.get("http://localhost:4200/")
 
-    def utest_invalid_login(self):
+    def test_invalid_login(self):
        
         self.driver.get("http://localhost:4200/login/")
 
@@ -34,7 +34,7 @@ class SearchText(unittest.TestCase):
         self.assertEqual(url_redirect, "http://localhost:4200/login")
 
     
-    def utest_valid_login(self):
+    def test_valid_login(self):
        
         self.driver.get("http://localhost:4200/login/")
 
@@ -53,24 +53,32 @@ class SearchText(unittest.TestCase):
 
         self.assertEqual(url_redirect, "http://localhost:4200/home")
 
-     def test_valid_login(self):
+    def test_home_before_login(self):
        
-        self.driver.get("http://localhost:4200/login/")
+        self.driver.get("http://localhost:4200/home/")
+        k = self.driver.find_elements_by_xpath("//app-header/div/ul/li")
+        #print("see:-   ",len(k))
+        self.assertEqual(len(k), 3)
 
-        self.search_field = self.driver.find_element_by_name("username")
-
-        self.search_field.send_keys("tester2")
-
-        self.search_field = self.driver.find_element_by_name("password")
-
-        self.search_field.send_keys("tester2")
-        self.search_field.submit()
-
+    def test_home_games_need_login(self):
        
+        self.driver.get("http://localhost:4200/home/")
+        k = self.driver.find_elements_by_xpath("//div[@class='bottomright']")
+       # print("see:-   ",len(k))
+        k[0].click()
         time.sleep(2)
         url_redirect = self.driver.current_url
+        self.assertEqual(url_redirect, "http://localhost:4200/login")
 
-        self.assertEqual(url_redirect, "http://localhost:4200/home")
+    def test_home_games_no_login(self):
+       
+        self.driver.get("http://localhost:4200/home/")
+        k = self.driver.find_elements_by_xpath("//div[@class='bottomright']")
+       # print("see:-   ",len(k))
+        k[2].click()
+        time.sleep(2)
+        url_redirect = self.driver.current_url
+        self.assertEqual(url_redirect, "http://localhost:4200/assets/Breakout_game/index.html")
 
     
 
